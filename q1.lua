ec0adbd5743fe557c64513962f126e9641ca88d8
Colter Snyder <coltersnyder@gmail.com>
2024-04-19 16:52:24 -0600

    Made Q1 into lambda function

--- q1.lua
+++ q1.lua
@@ -1,10 +1,8 @@
-local function releaseStorage(player)
-    player:setStorageValue(1000, -1)
-end
-    
 function onLogout(player)
     if player:getStorageValue(1000) == 1 then
-        addEvent(releaseStorage, 1000, player)
+        addEvent(function(player)
+            player:setStorageValue(1000, -1)
+        end, 1000, player)
     end
     return true
 end
